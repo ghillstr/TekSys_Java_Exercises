c0acae720c00a1d8824ed4c5529da5f9a7265092
George <ghillstr56@gmail.com>
2022-01-04 15:56:55 -0500

    little edits

--- Slide questions/src/com/pnc/ExercisesVariables.java
+++ Slide questions/src/com/pnc/ExercisesVariables.java
@@ -2,88 +2,85 @@ package com.pnc;
 
 public class ExercisesVariables {
 	public static void main(String[] args) {
-		
-		
-		//1
-		
+
+		// 1
+
 		int a = 1;
 		int b = 1;
-		
-		System.out.println(a + b);
-		
-		
-	 //2 
-		int c = 1;
-		int d = 1;
-		int sum = 0;
+		int sum1 = a + b;
+
+		System.out.println(sum1);
+
+		// 2
+		double c = 1.5;
+		double d = 1.6;
+		double sum = 0;
 		sum = c + d;
 		System.out.println(sum);
-	 
-		
-	//3
+
+		// 3
 		double ab = 2.09;
 		int cd = 3;
 		double sum3 = 0;
 		double abDouble = cd;
-		
+
 		sum3 = abDouble + ab;
-		
+
 		System.out.println(sum3);
-		
+
 		// What variable type must the sum be? Double
-		
-	//4
-		double x =  4.9;
+
+		// 4
+		double x = 4.9;
 		int y = 7;
 		double result = 0;
-		result = x/y;
+		result = x / y;
 		System.out.println(result);
-	
-		//What happens? What corrections are needed? Error, needs to be casted to a double
-		
-	//5 
+
+		// What happens? What corrections are needed? Error, needs to be casted to a
+		// double
+
+		// 5
 		double g = 97;
 		double h = 9;
-		double results1 = g/h;
+		double results1 = g / h;
 		System.out.println(results1);
 		int intResults1 = (int) results1;
 		System.out.println(intResults1);
-	
-	//6
+
+		// 6
 		int x2 = 5;
 		int y2 = 6;
-		
-		int q = y2/x2;
+
+		int q = y2 / x2;
 		System.out.println(q);
-		
+
 		double y2Double = (double) y2;
-		
-		double q2 = y2Double/x2;
+
+		double q2 = y2Double / x2;
 		System.out.println(q2);
-		
-		
-	//7
+
+		// 7
 		final double PI = 3.14;
-		
+
 		int radius = 6;
-		
-		double circumference = radius*PI*2;
-		
+
+		double circumference = radius * PI * 2;
+
 		System.out.println(circumference);
-		
-	//8 
+
+		// 8
 		double coffee = 3.50;
 		double espresso = 1.50;
 		double latte = 4.50;
 		double subtotal = 0;
 		double totalSale = 0;
 		final double salesTax = .05;
-		
+
 		subtotal = (coffee * 3) + (espresso * 4) + (latte * 2);
-		//System.out.println(subtotal);
+		// System.out.println(subtotal);
 		totalSale = (subtotal * salesTax) + subtotal;
 		System.out.printf("Your Total is %.2f", totalSale);
-		
-		
+
 	}
 }
--- Slide questions/src/com/pnc/Questions.java
+++ Slide questions/src/com/pnc/Questions.java
@@ -4,42 +4,40 @@ import java.util.Scanner;
 
 public class Questions {
 	public static void main(String[] args) {
-		
-		//display time 
+
+		// display time
 		Scanner sc = new Scanner(System.in);
 		int inputSeconds = 0;
 		int resultMinutes = 0;
-		int resultSecR = 0; //Remaining seconds
+		int resultSecR = 0; // Remaining seconds
 		System.out.println("Enter seconds:");
 		inputSeconds = sc.nextInt();
 		resultMinutes = (inputSeconds / 60);
 		resultSecR = (inputSeconds % 60);
-		
+
 		System.out.println(resultMinutes + "min and " + resultSecR + "sec");
-		
-		//Converting Temperature
+
+		// Converting Temperature
 		double temp = 0;
 		double celsius = 0;
 		System.out.println("Enter temperature (F):");
 		temp = sc.nextDouble();
-		celsius =(temp - 32) * 0.5556;
+		celsius = (temp - 32) * 0.5556;
 		System.out.printf("%.2f (C) %n", celsius);
-		
-		
+
 		// Sales Tax
-		
+
 		double input = 0;
 		double salesTax = .08875;
 		double output = 0;
-		
+
 		System.out.println("Enter Amount: ");
 		input = sc.nextDouble();
 		output = input * salesTax;
 		System.out.println(output);
-		
-		
-		//Computing Loan Payments
-		
+
+		// Computing Loan Payments
+
 		double interestRate = 0;
 		int years = 0;
 		double loanAmount = 0;
@@ -50,10 +48,10 @@ public class Questions {
 		years = sc.nextInt();
 		System.out.println("Enter Loan Amount:");
 		loanAmount = sc.nextDouble();
-		monthlyPayment = (loanAmount * interestRate) / (1 -(1/ ((1 + interestRate)*years)));
+		monthlyPayment = (loanAmount * interestRate) / (1 - (1 / ((1 + interestRate) * years)));
 		System.out.println(monthlyPayment);
 		
-		
-		
+		sc.close();
+
 	}
 }
