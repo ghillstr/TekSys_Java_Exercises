c494befd650bb9297bc998d882e12054041cf57b
George <ghillstr56@gmail.com>
2022-01-10 17:02:04 -0500

    loopExercies

---
+++ Java Exersises/src/com/pnc/LoopExercies.java
@@ -0,0 +1,123 @@
+package com.pnc;
+
+import java.util.Scanner;
+
+public class LoopExercies {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		// question1();
+		// question2();
+		// question3();
+		// question4();
+		// question5();
+		// question6();
+		question7();
+		// question8();
+		// question9();
+	}
+
+	public static void question1() {
+		for (int i = 1; i < 11; i++) {
+			System.out.println(i);
+		}
+	}
+
+	public static void question2() {
+		int i = 0;
+		while (i < 100) {
+			i = i + 10;
+			System.out.println(i);
+
+		}
+	}
+
+	public static void question3() {
+		int i = 1;
+		do {
+			System.out.println(i);
+			i++;
+		} while (i < 11);
+	}
+
+	public static void question4() {
+		int i = 0;
+		for (i = 0; i < 101; i++) {
+			if (i >= 25 && i <= 75)
+				continue;
+
+			if (i % 5 == 0) {
+
+				System.out.println(i);
+
+			}
+
+		}
+	}
+
+	public static void question5() {
+		int i = 0;
+		for (i = 0; i < 101; i++) {
+			if (i > 50) {
+				break;
+			}
+			if (i % 5 == 0) {
+				System.out.println(i);
+			}
+		}
+
+	}
+
+	public static void question6() {
+		for (int i = 1; i < 3; i++) {
+			System.out.println("Week " + i + ": \n");
+			for (int j = 1; j < 6; j++) {
+				System.out.println("Day " + j + "\n");
+			}
+
+		}
+	}
+
+	public static void question7() {
+		int r = 0;
+		int sum = 0;
+		int i = 0;
+
+		for (i = 10; i <= 200; i++) {
+
+			r = i % 10;
+			i = i / 10;
+			sum = (sum * 10) + r;
+
+		}
+		if (i == sum) {
+			System.out.println(i + " ");
+		}
+
+	}
+
+	public static void question8() {
+		int n1 = 0;
+		int n2 = 1;
+		int n3;
+		int i = 0;
+		int count = 10;
+		System.out.print(n1 + ", " + n2);
+		do {
+			for (i = 2; i < count; ++i) {
+				n3 = n1 + n2;
+				System.out.print(", " + n3);
+				n1 = n2;
+				n2 = n3;
+			}
+		} while (n1 + n2 <= 50);
+	}
+
+	public static void question9() {
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 10; j++) {
+				System.out.println("Inner loop: i: " + i + ", j: " + j);
+			}
+		}
+	}
+
+}
--- Java Exersises/src/com/pnc/QuesitonsLoops.java
+++ Java Exersises/src/com/pnc/QuesitonsLoops.java
@@ -1,5 +1,88 @@
 package com.pnc;
 
+import java.util.*;
+
 public class QuesitonsLoops {
 
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		// slide22(sc);
+		slide23(sc);
+		// multiTableSlide50();
+		// slide52();
+
+		sc.close();
+
+	}
+
+	public static void slide22(Scanner sc) {
+		Random r = new Random();
+		while (true) {
+			int a = r.nextInt(100) + 1;
+			int b = r.nextInt(100) + 1;
+			int result = 0;
+			System.out.println("What is " + a + " + " + b + "?");
+			System.out.println("Enter solution: ");
+			if (!sc.hasNextInt()) {
+				sc.next();
+				break;
+			}
+			result = sc.nextInt();
+
+			if ((a + b) == result) {
+				System.out.println("Correct Answer");
+			} else {
+				System.out.println("Incorrect Answer");
+			}
+		}
+
+	}
+
+	public static void slide23(Scanner sc) {
+		Random r = new Random();
+		char input2;
+		do {
+			int num = r.nextInt(100) + 1;
+			int input = 0;
+
+			//System.out.println(num); 
+			while (num != input) {
+				System.out.println("Enter number: ");
+				input = sc.nextInt();
+
+				if (num > input) {
+					System.out.println("Too Low");
+
+				} else if (num < input) {
+					System.out.println("Too High");
+				}
+			}
+
+			System.out.println("Correct");
+			System.out.println("Would you like to play again? Yes (Y) or No (N): ");
+			input2 = sc.next().charAt(0);
+		} while (input2 == 'Y' || input2 == 'y');
+
+	}
+
+	public static void multiTableSlide50() {
+
+		for (int outter = 1; outter < 13; outter++) {
+			System.out.println();
+			for (int inner = 1; inner < 13; inner++) {
+				System.out.printf("%4d", (outter * inner));
+			}
+		}
+	}
+
+	public static void slide52() {
+		int years = 0;
+
+		for (double i = 10000; i < 20000; i = (i * 1.07)) {
+			years++;
+
+		}
+		System.out.println(years);
+
+	}
 }
