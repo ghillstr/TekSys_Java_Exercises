6823d3d8be7f270905bd8b6ba5c4f9c8cac31702
George <ghillstr56@gmail.com>
2022-02-01 15:46:42 -0500

    more scope issues

--- Java Exersises/src/com/pnc/FileReaderExercise.java
+++ Java Exersises/src/com/pnc/FileReaderExercise.java
@@ -13,46 +13,43 @@ public class FileReaderExercise {
 
 		try {
 			File file = new File("src/2701-0.txt");
-			BufferedReader input = new BufferedReader(new FileReader(file));  // read input file
-			FileWriter copy = new FileWriter("mobyDick.txt"); //output file
-			// TreeMap<String, Integer> words = new TreeMap<String, Integer>(); // declare maps
+			BufferedReader input = new BufferedReader(new FileReader(file)); // read input file
+			FileWriter copy = new FileWriter("mobyDick.txt"); // output file
+			// TreeMap<String, Integer> words = new TreeMap<String, Integer>(); // declare
+			// map
 			HashMap<String, Integer> words = new HashMap<String, Integer>();
 			String line;
 			while ((line = input.readLine()) != null) {
 
 				for (String word : line.toLowerCase().split("\\P{Alpha}+")) { // to lower case & spit by alpha
 																				// numerics
-					if (!words.containsKey(word)) {
-						words.put(word, 1); // if there is one word
-					} else {
+					if (words.containsKey(word)) {
 						int count = words.get(word); // count words
 						count++;
 						words.put(word, count);
+					} else {
+						words.put(word, 1); // if there is one word
 					}
 				}
-
-				// words.forEach((key, value) -> System.out.println(key + "\t" + "\t" + "\t" +
-				// value));
+				 
 			}
-			TreeMap<String, Integer> wordsSorted = new TreeMap<String, Integer>(words);
+			words.forEach((key, value) -> System.out.println(key + "\t" + "\t" + "\t" +
+					 value));
+			TreeMap<String, Integer> wordsSorted = new TreeMap<String, Integer>(words); // sort HashMap
 			wordsSorted.forEach((key, value) -> {
 				try {
-					copy.write(key + "\t" + value + " \n");
+					copy.write(key + "\t" + value + " \n"); // print to output file
 				} catch (IOException e) {
 					System.out.println("An error occurred.");
 					e.printStackTrace();
-
 				}
 
 			});
-
 			input.close();
 			copy.close();
 
 		} catch (FileNotFoundException e) {
 			throw new Exception("No File Found");
 		}
-
 	}
-
 }
