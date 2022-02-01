0ac9e083526bf64c6e2d796355b896b303babdbc
George <ghillstr56@gmail.com>
2022-02-01 13:38:58 -0500

    Filewriter

--- Java Exersises/src/com/pnc/FileReaderExercise.java
+++ Java Exersises/src/com/pnc/FileReaderExercise.java
@@ -1,48 +1,59 @@
 package com.pnc;
 
+import java.io.*;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.util.Scanner;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.TreeMap;
 
 public class FileReaderExercise {
 	public static void main(String[] args) throws Exception {
-		
+
 		try {
-			FileReader file = new FileReader("src/Planet-x.txt");
-			Scanner sc = new Scanner(file);
-			
-			while (sc.hasNext()) {
-				String planetX = sc.nextLine();
-				
-		
-		// HashMap<String, Integer> words = new HashMap<String, Integer>(); //declare
-		// maps
-		TreeMap<String, Integer> words = new TreeMap<String, Integer>();
+			File file = new File("src/2701-0.txt");
+			BufferedReader input = new BufferedReader(new FileReader(file));
+			@SuppressWarnings("resource")
+			FileWriter copy = new FileWriter("copy.txt");
 
-		for (String word : planetX.toLowerCase().split("\\P{Alpha}+")) { // to lower case & spit by alpha numerics
-			if (!words.containsKey(word)) {
-				words.put(word, 1); // if there is one word
-			} else {
-				int count = words.get(word); // count words
-				count++;
-				words.put(word, count);
-			}
-		}
+			String line;
+			while ((line = input.readLine()) != null) {
 
-		words.forEach((key, value) -> System.out.println(key + "\t" + "\t" + "\t" + value));
+				// HashMap<String, Integer> words = new HashMap<String, Integer>(); //declare
+				// maps
+				TreeMap<String, Integer> words = new TreeMap<String, Integer>();
+
+				for (String word : line.toLowerCase().split("\\P{Alpha}+")) { // to lower case & spit by alpha
+																				// numerics
+					if (!words.containsKey(word)) {
+						words.put(word, 1); // if there is one word
+					} else {
+						int count = words.get(word); // count words
+						count++;
+						words.put(word, count);
+					}
+				}
+
+				words.forEach((key, value) -> System.out.println(key + "\t" + "\t" + "\t" + value));
+
+				words.forEach((key, value) -> {
+					try {
+						copy.write(key + "\t" + value + " \n");
+					} catch (IOException e) {
+						System.out.println("An error occurred.");
+						e.printStackTrace();
+
+					}
+
+				});
+
 			}
-			sc.close();
+			input.close();
+
+		} catch (FileNotFoundException e) {
+			throw new Exception("No File Found");
 		}
-			catch(FileNotFoundException e){
-				throw new Exception("No File Found");
-			}
-		
-		
 
 	}
-	
-
-
 
 }
