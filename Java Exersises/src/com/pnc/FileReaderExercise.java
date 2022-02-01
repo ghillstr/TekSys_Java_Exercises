49ef193bce7ff5d523278d6fbb5909302a699ef5
George <ghillstr56@gmail.com>
2022-02-01 16:35:14 -0500

    close to final

--- Java Exersises/src/com/pnc/FileReaderExercise.java
+++ Java Exersises/src/com/pnc/FileReaderExercise.java
@@ -15,36 +15,39 @@ public class FileReaderExercise {
 			File file = new File("src/2701-0.txt");
 			BufferedReader input = new BufferedReader(new FileReader(file)); // read input file
 			FileWriter copy = new FileWriter("mobyDick.txt"); // output file
-			// TreeMap<String, Integer> words = new TreeMap<String, Integer>(); // declare
+			TreeMap<String, Integer> words = new TreeMap<String, Integer>(); // declare
 			// map
-			HashMap<String, Integer> words = new HashMap<String, Integer>();
+			// HashMap<String, Integer> words = new HashMap<String, Integer>();
 			String line;
 			while ((line = input.readLine()) != null) {
 
-				for (String word : line.toLowerCase().split("\\P{Alpha}+")) { // to lower case & spit by alpha
-																				// numerics
+				for (String word : line.toLowerCase().split("\\P{Alpha}+")) { // to lower case & spit by alpha numerics
+
 					if (words.containsKey(word)) {
 						int count = words.get(word); // count words
 						count++;
 						words.put(word, count);
 					} else {
 						words.put(word, 1); // if there is one word
+
 					}
 				}
-				 
+
 			}
-			words.forEach((key, value) -> System.out.println(key + "\t" + "\t" + "\t" +
-					 value));
-			TreeMap<String, Integer> wordsSorted = new TreeMap<String, Integer>(words); // sort HashMap
-			wordsSorted.forEach((key, value) -> {
+			words.forEach((key, value) -> System.out.println(key + "\t" + value));
+
+			words.forEach((key, value) -> {
 				try {
+
 					copy.write(key + "\t" + value + " \n"); // print to output file
+
 				} catch (IOException e) {
 					System.out.println("An error occurred.");
 					e.printStackTrace();
 				}
 
 			});
+
 			input.close();
 			copy.close();
 
