76ad809b92c393159aa9fa80206e079f14474f35
George <ghillstr56@gmail.com>
2022-02-01 14:42:39 -0500

    fixed scope issue

--- Java Exersises/src/com/pnc/FileReaderExercise.java
+++ Java Exersises/src/com/pnc/FileReaderExercise.java
@@ -5,23 +5,20 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.TreeMap;
 
 public class FileReaderExercise {
 	public static void main(String[] args) throws Exception {
 
 		try {
-			File file = new File("src/Planet-x.txt");
-			BufferedReader input = new BufferedReader(new FileReader(file));
-			@SuppressWarnings("resource")
-			FileWriter copy = new FileWriter("PlanetX.txt");
-
+			File file = new File("src/2701-0.txt");
+			BufferedReader input = new BufferedReader(new FileReader(file));  // read input file
+			FileWriter copy = new FileWriter("mobyDick.txt"); //output file
+			// TreeMap<String, Integer> words = new TreeMap<String, Integer>(); // declare maps
+			HashMap<String, Integer> words = new HashMap<String, Integer>();
 			String line;
 			while ((line = input.readLine()) != null) {
-
-				// HashMap<String, Integer> words = new HashMap<String, Integer>(); //declare
-				// maps
-				TreeMap<String, Integer> words = new TreeMap<String, Integer>();
 
 				for (String word : line.toLowerCase().split("\\P{Alpha}+")) { // to lower case & spit by alpha
 																				// numerics
@@ -34,21 +31,23 @@ public class FileReaderExercise {
 					}
 				}
 
-				words.forEach((key, value) -> System.out.println(key + "\t" + "\t" + "\t" + value));
-
-				words.forEach((key, value) -> {
-					try {
-						copy.write(key + "\t" + value + " \n");
-					} catch (IOException e) {
-						System.out.println("An error occurred.");
-						e.printStackTrace();
-
-					}
-
-				});
-
+				// words.forEach((key, value) -> System.out.println(key + "\t" + "\t" + "\t" +
+				// value));
 			}
+			TreeMap<String, Integer> wordsSorted = new TreeMap<String, Integer>(words);
+			wordsSorted.forEach((key, value) -> {
+				try {
+					copy.write(key + "\t" + value + " \n");
+				} catch (IOException e) {
+					System.out.println("An error occurred.");
+					e.printStackTrace();
+
+				}
+
+			});
+
 			input.close();
+			copy.close();
 
 		} catch (FileNotFoundException e) {
 			throw new Exception("No File Found");
